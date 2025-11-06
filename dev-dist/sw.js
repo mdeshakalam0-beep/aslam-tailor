adc22662aa0eda41c933c111bc7bed7ec3615a4e
Imran <mdeshakalam0@gmail.com>
2025-11-06 15:45:25 +0530

    Initial commit

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-86c9b217'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.kg3g4a4lqho"
+    "revision": "0.kvj4ad9gv0g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
